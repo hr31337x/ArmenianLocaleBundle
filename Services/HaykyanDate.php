140d24d9450287a986168c9e89d830d5bbd3c469
Tigran Azatyan <tigran@azatyan.info>
2016-06-12 10:32:15 +0400

    ok

--- Services/HaykyanDate.php
+++ Services/HaykyanDate.php
@@ -23,15 +23,13 @@ namespace Azatyan\ArmenianLocaleBundle\Services;
  * ստուգումներ կոնվերտեր http://haytomar.com/converter.php?l=am
  * ստուգումներ տարիների աղյուսակ http://ru.wikipedia.org/wiki/%D0%94%D1%80%D0%B5%D0%B2%D0%BD%D0%B5%D0%B0%D1%80%D0%BC%D1%8F%D0%BD%D1%81%D0%BA%D0%B8%D0%B9_%D0%BA%D0%B0%D0%BB%D0%B5%D0%BD%D0%B4%D0%B0%D1%80%D1%8C
  * ------------------------ՕԳՏԱԳՈՐԾՈՒՄ-----------------------
- * ------------------------1․ ԿՈՆՍՏՐՈՒԿՏՈՐՆԵՐ-----------------
- * 1.1
- * $ArmDate = new HaykyanDate(1)
+ * 1) $date = new HaykyanDate(1)
  * կառուցում է ամսաթվի օբյեկտը ներկա պահի համար
  * որտեղ պարամետրը ընդունում է 1 կամ 2 արժեքները, և հաշվարկները կատարվում են ՏԱՐԲԵՐԱԿ 1 կամ ՏԱՐԲԵՐԱԿ 2 -ին համապատասխան
- * 1.2
- * $ArmDate = new HaykyanDate("2013-10-16 11:12:40",1)
- * որտեղ առաջին արգումենտը MySQL ֆորմատով ամսաթիվն է, երկրորդը՝ հաշվարկի տարբերակը 1.3
- * $ArmDate = new HaykyanDate(2014,8,11,2,1);
+ *
+ * 2) $date = new HaykyanDate("2013-10-16 11:12:40",1)
+ * որտեղ առաջին արգումենտը MySQL ֆորմատով ամսաթիվն է, երկրորդը՝ հաշվարկի տարբերակը
+ * 3) $date = new HaykyanDate(2014,8,11,2,1);
  * որտեղ արգումենտնենրն են համապատասխանաբար տարեթիվ,ամիս,օր,ժամ և տարբերակ
  * 
  */
@@ -215,42 +213,37 @@ class HaykyanDate
         if (method_exists($this, $f = '__init'.$i)) {
             call_user_func_array([$this, $f], $a);
         }
-    }
 
-    /* @TODO */
-    public function __init1($v)
-    {
-        $this->setVariant($v);
-        $this->date = getdate();
-        $this->createDate();
-        return $this;
-    }
+        switch($i) {
+            case 2:
+                $this->setVariant($a[0]);
+                $this->date = getdate(strtotime($a[1]));
+                $this->createDate();
+                break;
 
-    /* @TODO */
-    public function __init2($dateMySql, $v)
-    {
-        $this->setVariant($v);
-        $this->date = getdate(strtotime($dateMySql));
-        $this->createDate();
-        return $this;
-    }
+            case 5:
+                $this->setVariant($a[0]);
+                $this->date = getdate(mktime($a[1], 00, 00, $a[2], $a[3], $a[4]));
+                $this->createDate();
+                break;
+            default:
+                $this->setVariant($a[0]);
+                $this->date = getdate();
+                $this->createDate();
+                break;
+        }
 
-    /* @TODO */
-    public function __init5($year, $month, $day, $hour, $v)
-    {
-        $this->setVariant($v);
-        $this->date = getdate(mktime($hour, 00, 00, $month, $day, $year));
-        $this->createDate();
         return $this;
+
     }
 
     /**
-     * @param $tarb
+     * @param $variant
      * Ընտրում է հաշվարկների տարբերակներից, տես սքրիփթի սկիզբը
      */
-    private function setVariant($tarb)
+    private function setVariant($variant)
     {
-        if ($tarb == 1) {
+        if ($variant == 1) {
             //Տարբերակ 1 -ի համար
             $this->epYr = 2492; //մթա
             $this->yrM = 8;
